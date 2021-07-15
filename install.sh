44851434ec722263b6e1293e9f9b6459bb2d01d5
Carlos Orama <solazdev@gmail.com>
2021-07-14 22:41:10 -0400

    Update to 4.1.1

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-wget -O tizen_studio.bin http://download.tizen.org/sdk/Installer/tizen-studio_3.7/web-ide_Tizen_Studio_3.7_ubuntu-64.bin
+wget -O tizen_studio.bin https://usa.sdk-dl.tizen.org/web-ide_Tizen_Studio_4.1.1_usa_ubuntu-64.bin
 chmod +x tizen_studio.bin
 PATH=$(pwd):$PATH ./tizen_studio.bin
